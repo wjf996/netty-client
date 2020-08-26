4da17649596a57709fe490cfb8187f58d07dfd1c
wjf <1589011252@qq.com>
2020-08-26 14:34:40 +0800

    netty初始化客户端

--- netty-client/src/main/groovy/com/netty/client/netty/client/NettyClient.groovy
+++ netty-client/src/main/groovy/com/netty/client/netty/client/NettyClient.groovy
@@ -7,16 +7,17 @@ import io.netty.channel.ChannelFuture
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
-import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.channel.socket.nio.NioSocketChannel
 import io.netty.handler.codec.string.StringDecoder
 import io.netty.handler.codec.string.StringEncoder
 
-import java.nio.channels.SocketChannel
 
 
 @Slf4j
 class NettyClient {
+
+
+
 
     void start(InetSocketAddress socketAddress) {
         def loopGroup = new NioEventLoopGroup()
